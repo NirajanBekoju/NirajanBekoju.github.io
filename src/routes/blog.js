debf271b30a7060a37de4dbc3f4feba80ba29ee6
Nirajan Bekoju <nirajan.bekoju@gmail.com>
2022-12-31 17:14:07 +0545

    few changes in no block message

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -8,7 +8,7 @@ function blog() {
                 <div className='row'>
                     <h1 className='p-0'>Blog</h1>
                     <hr/>
-                    {blogs_list.length > 0  ? "Blog" : "No Blogs yet"}
+                    {blogs_list.length > 0  ? "Blog" : "Will add soon..."}
                 </div>
             </div>
         </section>
