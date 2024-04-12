1165910bf8ddd3f0797a79661a438db18e5ffeef
Nirajan Bekoju <nirajan.bekoju@gmail.com>
2024-04-12 11:12:04 +0545

    add blogs
    - design pattern
    - factory design pattern

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,4 +1,6 @@
 import solidImage from "../assets/blogs/solid.png";
+import designPattern from "../assets/blogs/design_pattern.png"
+import factoryDesignPattern from "../assets/blogs/factory_design_pattern.png"
 
 const blogsList = [
   {
@@ -6,7 +8,21 @@ const blogsList = [
     title: "SOLID Design Pattern",
     description:
       "The SOLID principles are a set of five design principles that help software developers create more maintainable, flexible, and understandable code.",
-    link: "https://woozy-tanker-2da.notion.site/SOLID-Design-Pattern-f0d4f2bd20e24d52a97ea5fefc3ab728",
+    link: "https://nirajanbekoju.notion.site/SOLID-Design-Pattern-aa2ba26142294521b9f40346c020249a?pvs=4",
+  },
+  {
+    image: designPattern,
+    title: "Design Pattern",
+    description:
+      "A software design pattern is a general, reusable solution to a commonly occurring problem within a given context in software design.",
+    link: "https://nirajanbekoju.notion.site/Design-Pattern-62202710a12c4997a8052178225185ee?pvs=4",
+  },
+  {
+    image: factoryDesignPattern,
+    title: "Factory Design Pattern",
+    description:
+      "The factory method pattern is a creational pattern that uses factory methods to deal with the problem of creating objects without having to specify the exact class of the object that will be created.",
+    link: "https://nirajanbekoju.notion.site/Factory-Design-Pattern-a0fb4d82ef464d628b523db2ab49b29b?pvs=4",
   },
 ];
 
