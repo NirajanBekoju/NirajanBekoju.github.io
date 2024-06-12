501b27c94f52303bb14119fda4aeb49075848e2a
NirajanBekoju <nirajan.bekoju@gmail.com>
2024-06-12 10:02:45 +0545

    add sequential recommender system blogs

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -4,6 +4,9 @@ import factoryDesignPattern from "../assets/blogs/factory_design_pattern.png";
 import singletonDesignPattern from "../assets/blogs/singleton_design_pattern.png";
 import pylint from "../assets/blogs/pylint.png";
 import logging from "../assets/blogs/logging.png"
+import sequential_rec_sys_image from "../assets/blogs/sequential_rec_sys.png"
+import recbole_image from "../assets/blogs/recbole.png"
+import learning_to_rank_image from "../assets/blogs/learning_to_rank.png"
 
 const blogsList = [
   {
@@ -47,6 +50,27 @@ const blogsList = [
     description:
       "Singleton is a creational design pattern that lets you ensure that a class has only one instance while providing a global access point to this instance.",
     link: "https://nirajanbekoju.notion.site/Singleton-Design-Pattern-e5fe67d5ff6e41e4a87d2dd755fb3253",
+  },
+  {
+    image: sequential_rec_sys_image,
+    title: "Sequential Recommender System",
+    description:
+      "Sequential Recommendation System takes the prior sequential interactions as a context to predict which items would be interacted in the near future.",
+    link: "https://www.notion.so/nirajanbekoju/Sequential-Recommender-System-Challenges-Progress-and-Prospects-11ffcfbce6634de196e68bc3b063f3b4",
+  },
+  {
+    image: recbole_image,
+    title: "RecBole",
+    description:
+      "RecBole is developed based on Python and PyTorch for reproducing and developing recommendation algorithms in a unified, comprehensive and efficient framework for research purpose.",
+    link: "https://nirajanbekoju.notion.site/RecBole-0d78672484874a25a4ecee71e3a4bd95",
+  },
+  {
+    image: learning_to_rank_image,
+    title: "Learning to Rank",
+    description:
+      "Learning to rank is concerned with a model or a function for ranking objects which is useful for various applications like document retrieval and collaborative filtering.",
+    link: "https://nirajanbekoju.notion.site/Learning-to-Rank-From-Pairwise-Approach-to-Listwise-Approach-67f5271917dc4502a09d64d1de681262",
   },
 ];
 
